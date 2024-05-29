e19ebf5a3a55471a6315754765eed5a247d406b9
User <fadhillatmojo@gmail.com>
2024-05-29 12:21:53 +0700

    fix: try catch

--- app/Http/Controllers/DonorsController.php
+++ app/Http/Controllers/DonorsController.php
@@ -2,47 +2,77 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\BloodDonation;
-use Illuminate\Http\Request;
+use Exception;
 use App\Models\Donor;
 use Faker\Core\Blood;
+use Illuminate\Http\Request;
+use App\Models\BloodDonation;
 
 class DonorsController extends Controller
 {
     public function getDonors()
     {
-        $donors = Donor::all();
-        
-        // Donor::join('blood_donations', 'donors.donor_id', '=', 'blood_donations.donor_id')
-        //     ->select('donors.*', 'blood_donations.donor_date')
-        //     ->get();
-        return response()->json($donors);
+        try {
+            $donors = Donor::all();
+
+            // Donor::join('blood_donations', 'donors.donor_id', '=', 'blood_donations.donor_id')
+            //     ->select('donors.*', 'blood_donations.donor_date')
+            //     ->get();
+            return response()->json($donors);
+
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $e->getMessage()
+            ], 404);
+        }
     }
 
     public function getDonorById($id)
     {
-        $donor = Donor::where('donor_id', $id)->get();
-        return response()->json($donor);
+        try {
+            $donor = Donor::where('donor_id', $id)->get();
+            return response()->json($donor);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $e->getMessage()
+            ], 404);
+        }
     }
 
     public function createDonor(Request $request)
     {
-        $donor = new Donor;
-        $donor->name = $request->name;
-        $donor->gender= $request->gender;
-        $donor->blood_type = $request->blood_type;
-        $donor->rhesus_type = $request->rhesus_type;
-        $donor->date_of_birth = $request->date_of_birth;
-        $donor->address = $request->address;
-        $donor->phone_number = $request->phone_number;
-        $donor->save();
+        try {
+            $donor = new Donor;
+            $donor->name = $request->name;
+            $donor->gender = $request->gender;
+            $donor->blood_type = $request->blood_type;
+            $donor->rhesus_type = $request->rhesus_type;
+            $donor->date_of_birth = $request->date_of_birth;
+            $donor->address = $request->address;
+            $donor->phone_number = $request->phone_number;
+            $donor->save();
 
-        return response()->json($donor);
+            return response()->json($donor);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $e->getMessage()
+            ], 404);
+        }
     }
 
     public function getDonorsAll()
     {
-        $donor = Donor::all();
-        return response()->json($donor);
+        try {
+            $donor = Donor::all();
+            return response()->json($donor);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $e->getMessage()
+            ], 404);
+        }
     }
 }
